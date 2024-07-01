9bd993fe944e3921d017193f3ec24246c2e68d09
suyash <suyashmishra145.com>
2024-07-01 13:23:39 +0530

    Adding migrations and db files

---
+++ my-blogging-app/prisma/main.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: 'alice123',
+        password: 'Ronaldo312'
+      },
+    });
+    console.log('User created:', user);
+  } catch (error) {
+    console.error('Error creating user:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main();
